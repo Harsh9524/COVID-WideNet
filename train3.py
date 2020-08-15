f5846e83e5028147db64a49e87a39fc693cd00df
Harsh Panwar <harshpanwar9524@gmail.com>
2020-08-16 00:35:50 +0530

    Update train3.py

--- train3.py
+++ train3.py
@@ -53,11 +53,7 @@ def CapsNet(input_shape, n_class, routings):
     eval_model = models.Model(x, [out_caps, decoder(masked)])
 
     # manipulate model
-    noise = layers.Input(shape=(n_class, 16))
-    noised_digitcaps = layers.Add()([digitcaps, noise])
-    masked_noised_y = Mask()([noised_digitcaps, y])
-    manipulate_model = models.Model([x, y, noise], decoder(masked_noised_y))
-    return train_model, eval_model, manipulate_model
+    return train_model, eval_model
 
 
 def margin_loss(y_true, y_pred):
@@ -107,7 +103,24 @@ def train(model, data, args):
     # Begin: Training with data augmentation ---------------------------------------------------------------------#
     def train_generator(x, y, batch_size, shift_fraction=0.):
         train_datagen = ImageDataGenerator(width_shift_range=shift_fraction,
-                                           height_shift_range=shift_fraction)  # shift up to 2 pixel for MNIST
+                                           height_shift_range=shift_fraction,
+                                           featurewise_center=True,
+                                           samplewise_center=False, 
+                                           featurewise_std_normalization=True,
+                                           samplewise_std_normalization=False, 
+                                           zca_whitening=False, 
+                                           zca_epsilon=1e-06, 
+                                           rotation_range=0.1,
+                                           shear_range=0.0, 
+                                           zoom_range=0.1, 
+                                           channel_shift_range=0.0, 
+                                           fill_mode='nearest', 
+                                           cval=0.0, 
+                                           horizontal_flip=True,
+                                           vertical_flip=False, 
+                                           rescale=None, 
+                                           preprocessing_function=None,
+                                           data_format=None)  # shift up to 2 pixel for MNIST
         generator = train_datagen.flow(x, y, batch_size=batch_size)
         while 1:
             x_batch, y_batch = generator.next()
@@ -146,29 +159,29 @@ def test(model, data, args):
     plt.show()
 
 
-def manipulate_latent(model, data, args):
-    print('-'*30 + 'Begin: manipulate' + '-'*30)
-    x_test, y_test = data
-    index = np.argmax(y_test, 1) == args.digit
-    number = np.random.randint(low=0, high=sum(index) - 1)
-    x, y = x_test[index][number], y_test[index][number]
-    x, y = np.expand_dims(x, 0), np.expand_dims(y, 0)
-    noise = np.zeros([1, 10, 16])
-    x_recons = []
-    for dim in range(16):
-        for r in [-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25]:
-            tmp = np.copy(noise)
-            tmp[:,:,dim] = r
-            x_recon = model.predict([x, y, tmp])
-            x_recons.append(x_recon)
+# def manipulate_latent(model, data, args):
+#     print('-'*30 + 'Begin: manipulate' + '-'*30)
+#     x_test, y_test = data
+#     index = np.argmax(y_test, 1) == args.digit
+#     number = np.random.randint(low=0, high=sum(index) - 1)
+#     x, y = x_test[index][number], y_test[index][number]
+#     x, y = np.expand_dims(x, 0), np.expand_dims(y, 0)
+#     noise = np.zeros([1, 10, 16])
+#     x_recons = []
+#     for dim in range(16):
+#         for r in [-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25]:
+#             tmp = np.copy(noise)
+#             tmp[:,:,dim] = r
+#             x_recon = model.predict([x, y, tmp])
+#             x_recons.append(x_recon)
 
-    x_recons = np.concatenate(x_recons)
+#     x_recons = np.concatenate(x_recons)
 
-    img = combine_images(x_recons, height=16)
-    image = img*255
-    Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % args.digit)
-    print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.digit))
-    print('-' * 30 + 'End: manipulate' + '-' * 30)
+#     img = combine_images(x_recons, height=16)
+#     image = img*255
+#     Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % args.digit)
+#     print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.digit))
+#     print('-' * 30 + 'End: manipulate' + '-' * 30)
 
 
 def load_covid():
@@ -214,8 +227,8 @@ if __name__ == "__main__":
     parser.add_argument('--save_dir', default='./result')
     parser.add_argument('-t', '--testing', action='store_true',
                         help="Test the trained model on testing dataset")
-    parser.add_argument('--digit', default=5, type=int,
-                        help="Digit to manipulate")
+    # parser.add_argument('--digit', default=5, type=int,
+    #                     help="Digit to manipulate")
     parser.add_argument('-w', '--weights', default=None,
                         help="The path of the saved weights. Should be specified when testing")
     args = parser.parse_args()
@@ -228,9 +241,9 @@ if __name__ == "__main__":
     x_train, y_train, x_test, y_test = load_covid()
 
     # define model
-    model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
-                                                  n_class=len(np.unique(np.argmax(y_train, 1))),
-                                                  routings=args.routings)
+    model, eval_model= CapsNet(input_shape=x_train.shape[1:],
+                               n_class=len(np.unique(np.argmax(y_train, 1))),
+                               routings=args.routings)
     model.summary()
 
     # train or test
@@ -241,6 +254,6 @@ if __name__ == "__main__":
     else:  # as long as weights are given, will run testing
         if args.weights is None:
             print('No weights are provided. Will test using random initialized weights.')
-        manipulate_latent(manipulate_model, (x_test, y_test), args)
+#         manipulate_latent(manipulate_model, (x_test, y_test), args)
         test(model=eval_model, data=(x_test, y_test), args=args)
 
